34c2884690672ce5e9ec566ee8654315d31921c7
naoya sakuma <4869ns@gmail.com>
2020-11-10 22:54:21 +0900

    バリデーションを設定

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,5 @@
 class Album < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
