05cb66d1f815ed9a9f9637d7f021c92116510ed9
naoya sakuma <4869ns@gmail.com>
2020-11-10 23:00:16 +0900

    バリデーションを変更

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
+  before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
