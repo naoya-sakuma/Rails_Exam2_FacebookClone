885ed9cd3db351ff34a864a40cf33f3381254fc2
naoya sakuma <4869ns@gmail.com>
2020-11-11 11:20:31 +0900

    セッション機能を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     album = Album.find_by(email: params[:session][:email].downcase)
     if album && album.authenticate(params[:session][:password])
-
+      session[:album_id] = album.id
+      redirect_to album_path(album.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
